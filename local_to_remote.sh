d573cf28181db96f55cc232db61312683ad4c6d9
WyattJia <i@eventloop.live>
2021-08-02 09:59:28 +0800

    Update sync scripts.

--- local_to_remote.sh
+++ local_to_remote.sh
@@ -2,8 +2,7 @@ cp ../.alias .
 cp ../.bash_profile .
 cp ../.emacs .
 cp ../.vimrc .
-cp ../.zprofile .
 cp ../.zshrc .
 
-cp -r ~/.config/i3 .
-cp -r ~/.config/polybar .
+# cp -r ~/.config/i3 .
+
--- remote_to_local.sh
+++ remote_to_local.sh
@@ -2,8 +2,7 @@ cp .alias                ~/.alias
 cp .bash_profile         ~/.bash_profile
 cp .emacs                ~/.emacs
 cp .vimrc                ~/.vimrc
-cp .zprofile             ~/.zprofile
 cp .zshrc                ~/.zshrc
 
-cp -r i3/                ~/.config/
-cp -r polybar/           ~/.config/
+# cp -r i3/                ~/.config/
+
